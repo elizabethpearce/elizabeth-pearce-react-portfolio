0fc28ac1aa7e192ff4a91800ab949d83afbed431
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-03-16 23:21:58 -0500

    Contact Page Finalization

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -4,12 +4,26 @@ import {
   faEdit, faSpinner,
   faCircleNotch, faBahai,
   faPlusSquare,
-  faCirclePlus
+  faCirclePlus,
+  faPhone,
+  faEnvelope,
+  faMapMarkedAlt
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons =() => {
-  return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faCircleNotch, faBahai, faPlusSquare, faCirclePlus);
+  return library.add(
+    faTrash,
+    faSignOutAlt,
+    faEdit, faSpinner,
+    faCircleNotch,
+    faBahai,
+    faPlusSquare,
+    faCirclePlus,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+    );
 };
 
 export default Icons;
