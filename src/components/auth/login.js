f0044275d60e6fd850c8cb3020addb3ca17735a7
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-03-19 20:23:43 -0500

    finalized portfolio

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
   constructor(props) {
@@ -56,7 +57,9 @@ export default class Login extends Component {
 
         <div>{this.state.errorText}</div>
 
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+            <FontAwesomeIcon icon="envelope" />
           <input
             type="email"
             name="email"
@@ -64,7 +67,10 @@ export default class Login extends Component {
             value={this.state.email}
             onChange={this.handleChange}
           />
+           </div>
 
+           <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
           <input
             type="password"
             name="password"
@@ -72,10 +78,10 @@ export default class Login extends Component {
             value={this.state.password}
             onChange={this.handleChange}
           />
-
-          <div>
-            <button ClassName="btn" type="submit">Login</button>
           </div>
+
+          <button className="btn" type="submit">Login</button>
+  
         </form>
       </div>
     );
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -7,7 +7,8 @@ import {
   faCirclePlus,
   faPhone,
   faEnvelope,
-  faMapMarkedAlt
+  faMapMarkedAlt,
+  faLock
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
@@ -22,7 +23,8 @@ const Icons =() => {
     faCirclePlus,
     faPhone,
     faEnvelope,
-    faMapMarkedAlt
+    faMapMarkedAlt,
+    faLock
     );
 };
 
